09a04aad88497f6ae8066a2adfec9d3eb8848938
wjdals3936 <101387993+wjdals3936@users.noreply.github.com>
2022-05-28 21:52:32 +0900

    Update and rename hello python to hello python.py
    log

--- hello python.py
+++ hello python.py
@@ -1,5 +1,5 @@
-Jungmin Park, Hankuk University of foreign Studies
-Touches by user2
+print("Jungmin Park, Hankuk University of foreign Studies")
+print("Touches by user2")
 
 n = int(input())
 
@@ -9,4 +9,4 @@ print("hello python")
 
 #안녕하세요저는두번째과제를수행중입니다삐빅
 #깃허브이것저것다신기하다삐비비빅
-파이썬 공부 얍얍ㅇ뱌
+#파이썬 공부 얍얍ㅇ뱌
